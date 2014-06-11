f016b5405e1081fa4616a26cb89daa622b8d0e71
Radovan Janjic <rade@it-radionica.com>
2014-06-11 17:01:58 +0200

    Update mysql_wrapper.sh

--- mysql_wrapper.sh
+++ mysql_wrapper.sh
@@ -34,6 +34,15 @@ newline='\n';
 # Column names as first row
 column_names=1;
 
+# backup
+backup_path='backups';
+
+# result
+num_rows=0;
+num_columns=0;
+# fetch return
+declare -A matrix;
+
 ###################################################################################
 # FUNCTION
 ###################################################################################
@@ -85,6 +94,78 @@ query() {
 ###################################################################################
 
 #
+# query fetch first result
+#
+# $1 -query
+fetch1st() {
+	local i=0;
+	local arr=();
+	num_rows=0;
+	num_columns=0;
+	while read -r line; do
+		if [ $i -gt 0 ]; then
+			#echo "$line" | cut -f1; # column 1
+			#echo "$line" | cut -f2; # column 2
+			#echo "$line" | cut -f3; # column 3
+			# column ...
+			local j=1;
+			for k in ${arr[@]}; do
+				#num_columns=`expr $num_columns + 1`;
+				eval matrix[$k]='$(echo "$line" | cut -f$j)';
+				j=`expr $j + 1`;
+			done
+			num_rows=$i;
+		else
+			local arr=(${line// / });
+			for k in ${arr[@]}; do
+				num_columns=`expr $num_columns + 1`;
+				echo $k
+				
+				#echo $num_columns
+			done
+		fi
+		i=`expr $i + 1`;
+	done <<< "$(query "$(echo "$1" | sed -e 's/;$//;s/limit\s*[0-9]*\s*$//Ig;s/limit\s*[0-9]*\s*,\s*[0-9]*\s*$//Ig;') LIMIT 1;")"
+}
+
+#
+# query fetch result into array
+#
+# $1 -query
+fetch2array() {
+	num_rows=0;
+	num_columns=0;
+	i=0;
+	while read -r line; do
+		if [ $i -gt 0 ]; then
+			for (( j=1; j<=num_columns; j++ )); do
+				eval matrix[$i,$j]='$(echo "$line" | cut -f$j)';
+			done
+			num_rows=$i;
+		else
+			local arr=(${line// / });
+			for k in ${arr[@]}; do
+				num_columns=`expr $num_columns + 1`;
+			done
+		fi
+		i=`expr $i + 1`;		
+	done <<< "$(query "$1")" 
+}
+
+#
+# example array2insert
+#
+#fetch2array "select * from test limit 5";
+#for (( j=1; j <= num_columns; j++ )); do
+#    for (( i=1; i <= num_rows; i++ )); do
+#		 # $i - row
+#		 # $j - col
+#        echo ${matrix[$i,$j]}
+#    done
+#done
+###################################################################################
+
+#
 # insert values into table
 #
 # $1 - table name
@@ -110,12 +191,12 @@ array2insert() {
 	done
 
 	# do query
-	query "INSERT INTO \`$1\` ( $(join ',' "${columns[@]}") ) VALUES ( $(join ',' "${values[@]}") ); SELECT LAST_INSERT_ID();" | while read -r line; do
+	while read -r line; do
 		if [ $i -gt 0 ]; then
 			last_insert_id=$(echo "$line" | cut -f1);
 		fi
 		i=`expr $i + 1`;
-	done
+	done <<< "$(query "INSERT INTO \`$1\` ( $(join ',' "${columns[@]}") ) VALUES ( $(join ',' "${values[@]}") ); SELECT LAST_INSERT_ID();")" 
 }
 
 #
@@ -152,12 +233,12 @@ array2update() {
 	[ -z "$3" ] || where=' WHERE '$3;
 
 	# do query
-	query "UPDATE \`$1\` SET $(join ',' "${columns[@]}")"$where"; SELECT ROW_COUNT();" | while read -r line; do
+	while read -r line; do
 		if [ $i -gt 0 ]; then
 			affected_rows=$(echo "$line" | cut -f1);
 		fi
 		i=`expr $i + 1`;
-	done
+	done <<< "$(query "UPDATE \`$1\` SET $(join ',' "${columns[@]}")"$where"; SELECT ROW_COUNT();")" 
 }
 
 #
@@ -178,17 +259,17 @@ array2update() {
 query2csv() {
 	# real path
 	local file=$(readlink -f $2);
-	
+
 	# remove CSV file if exists
 	[ -f "$file" ] && rm $file;
-	
+
 	# remove ; from end of query 
 	local sql=$(echo "$1" | sed "s/;$//");
 	# remove ; from end of query and replace limit with limit 1 (used for column names query)
 	local sqlc=$(echo "$1" | sed -e 's/;$//;s/limit\s*[0-9]*\s*$//Ig;s/limit\s*[0-9]*\s*,\s*[0-9]*\s*$//Ig;');
 	# output to file sql
 	local sql_out=" INTO OUTFILE '"$(escape $file)"' FIELDS TERMINATED BY '"$delimiter"' OPTIONALLY ENCLOSED BY '"$enclosure"' ESCAPED BY '"$escape"' LINES TERMINATED BY '"$newline"';";
-	
+
 	if [ $column_names -gt 0 ]; then
 		local i=0;
 		columns_sql="SELECT ";
@@ -226,4 +307,36 @@ table2csv(){
 # example table2csv
 #
 #table2csv 'test' 'test.csv'
+###################################################################################
+
+
+#
+# backup (no args will backup all databases)
+#
+# $1 - database name [optional]
+# $2 - table name [optional]
+backup() {
+	local path='';
+	path=$(readlink -f $backup_path);
+	mkdir -p $path;
+	if [ -z "$1" ] && [ -z "$2" ]; then
+		mysqldump -f --opt -u$con_user -p$con_pass -h$con_host -P$con_port --all-databases > $path/`date +%Y%m%d`.sql
+		gzip $path/`date +%Y%m%d`.sql
+	else
+		if [ ! -z "$2" ]; then
+			mysqldump -f --opt -u$con_user -p$con_pass -h$con_host -P$con_port $1 $2 > $path/`date +%Y%m%d`.$1-$2.sql
+			gzip $path/`date +%Y%m%d`.$1-$2.sql
+		else
+			mysqldump -f --opt -u$con_user -p$con_pass -h$con_host -P$con_port $1 > $path/`date +%Y%m%d`.$1.sql
+			gzip $path/`date +%Y%m%d`.$1.sql
+		fi
+	fi
+}
+
+#
+# example backup
+#
+#backup                        # backup all databases
+#backup 'testdb'               # backup testdb
+#backup 'testdb' 'test_table'  # backup test_table
 ###################################################################################
